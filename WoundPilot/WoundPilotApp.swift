1907a2070519ffb84819613d8d334be40bbabc7f
agatalangova17 <agata.langova@praguecollege.cz>
2025-06-09 16:25:15 +0200

    Initial Commit

---
+++ WoundPilot/WoundPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoundPilotApp.swift
+//  WoundPilot
+//
+//  Created by Agata Langova on 09/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WoundPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
